bb907bd769870b2eda5eedc52629a6dcde101690
lmarianbm <luzbandres@gmail.com>
2023-05-18 17:42:34 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector("button.boton-menu-toggle")
+.addEventListener("click", function() {
+document.querySelector(".nav-links").classList.toggle("nav-links-responsive")
+})
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+    }
+    function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+    alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+    alert('Correo electrónico enviado correctamente.');
+    }
+    }
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+validateForm();
+    })
+function showAlert(){ alert("¡Hola, esta es una alerta desde JavaScript!");} 
